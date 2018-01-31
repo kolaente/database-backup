a62eb920712e759217b782fa184a3a650aaf009f
kolaente <konrad@kola-entertainments.de>
2018-01-31 14:40:04 +0100

    edited runscript to show cron config on startup

--- run.sh
+++ run.sh
@@ -7,6 +7,6 @@ if [ -n "${NO_CRON}" ]; then
 else
     echo "${CRON_TIME} /backup.sh" > /crontab.conf
     crontab  /crontab.conf
-    echo "=> Running cron job"
+    echo "Running cron job. Config: ${CRON_TIME}"
     exec crond -f
 fi
